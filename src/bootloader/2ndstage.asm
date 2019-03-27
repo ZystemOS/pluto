d03397c7a21e3b9c6d40843c9d0c79c116b81734
ED <ed.dean515@gmail.com>
2019-03-27 19:52:34 +0000

    Changed to spaces from tabs

--- src/bootloader/2ndstage.asm
+++ src/bootloader/2ndstage.asm
@@ -1,27 +1,26 @@
 ; ------------------------------------------------------------
 ; Stage 2 of the bootloader
 ; ------------------------------------------------------------
-%define boot_sector_location	(0x7C03)	; The location of the boot sector
-%define fat_segment				(0x0050)	; The memory location to load the FAT into memory
-;%define stage_2_location		(0x2000)	; The location of the second stage bootloader
-%define stage_2_location		(0x7E00)
+%define boot_sector_location    (0x7C03)    ; The location of the boot sector
+%define fat_segment             (0x0050)    ; The memory location to load the FAT into memory
+%define stage_2_location        (0x7E00)    ; The location of the second stage bootloader
 
-%define kernel_stack			(0x9FBFF)	; The location of the start of the bottom of the stack
+%define kernel_stack            (0x9FBFF)   ; The location of the start of the bottom of the stack
 
-%define kernel_load_segment		(0x3000)	; The segment when the kernel is loaded by the bootloader before A20 is enabled so can access above 1MB
-%define kernel_load_location	(0x30000)	; The location when the kernel is loaded by the bootloader before A20 is enabled so can access above 1MB
-%define kernel_target_location	(0x100000)	; The target location for the kernel to be loaded. Above 1MB.
+%define kernel_load_segment     (0x3000)    ; The segment when the kernel is loaded by the bootloader before A20 is enabled so can access above 1MB
+%define kernel_load_location    (0x30000)   ; The location when the kernel is loaded by the bootloader before A20 is enabled so can access above 1MB
+%define kernel_target_location  (0x100000)  ; The target location for the kernel to be loaded. Above 1MB.
 
-%define memory_map_location		(0x20000)	; This is where the memory map is loaded into. At bottom of 2nd stage bootloader
-%define memory_map_segment		(0x02000)
+%define memory_map_location     (0x20000)   ; This is where the memory map is loaded into. At bottom of 2nd stage bootloader
+%define memory_map_segment      (0x02000)
 
-%define boot_params_location	(0x7000)	; The location where the boot parameters are save to for the kernel
-%define SIGNATURE				(0x8A3C)	; The signature of the parameters to test for valid parameters
+%define boot_params_location    (0x7000)    ; The location where the boot parameters are save to for the kernel
+%define SIGNATURE               (0x8A3C)    ; The signature of the parameters to test for valid parameters
 
-	[bits	16]					; Tell the assembler that this is a 16bit program not 32bit
-	[org	stage_2_location]
-
-	jmp		stage_2_bootload_start
+    [bits    16]                    ; Tell the assembler that this is a 16bit program not 32bit
+    [org    stage_2_location]
+    
+    jmp     stage_2_bootload_start
 
 times (3 - ($ - $$)) db 0
 
@@ -36,120 +35,119 @@ boot_sector:
 %include 'memory.asm'
 
 stage_2_bootload_start:
-	cli
-	xor		ax, ax
-	mov		ds, ax
-	mov		es, ax
-	sti
-
-	; Copy the boot sector
-	m_copy_boot_sector
-	
-	; Find the kernel file
-	m_find_file kernel_filename, kernel_load_segment
-	
-	; Read the kernel into the load segment
-	m_read_file kernel_load_segment, fat_segment
-	
-	; Save The size read, stored in BX, may change to number of sectors as kernel size gets bigger
-	; kernel_size is in bytes
-	mov		word [kernel_size], bx
-	mov		word [boot_parameters.kernel_len], bx
-	
-	; Reset the disk
-	m_reset_disk
-	
-	; Write the loading message for the second stage
-	m_write_line loading_msg
-	
-	; Save the the boot parameters so the kernel can access them
-	m_save_cursor
-	
-	; Enable the a20 line
-	m_enable_a20
-	
-	; Read the size of the memory and store at 'boot_info'
-	m_get_memory_size
-	
-	; Set up and write into memory the interrupt descriptor table
-	m_setup_idt
-	
-	; Set up and write into memory the global descriptor table
-	m_setup_gdt
-	
-	; Load the tables
-	m_load_gdt_and_idt
-	
-	; Enable protected mode
-	m_enable_protected
-	
-	jmp		0x08:stage_2_bootloader_32		; Set CS to the code segment of the kernel in the GDT
+    cli
+    xor     ax, ax
+    mov     ds, ax
+    mov     es, ax
+    sti
+    
+    ; Copy the boot sector
+    m_copy_boot_sector
+    
+    ; Find the kernel file
+    m_find_file kernel_filename, kernel_load_segment
+    
+    ; Read the kernel into the load segment
+    m_read_file kernel_load_segment, fat_segment
+    
+    ; Save The size read, stored in BX, may change to number of sectors as kernel size gets bigger
+    ; kernel_size is in bytes
+    mov     word [kernel_size], bx
+    mov     word [boot_parameters.kernel_len], bx
+    
+    ; Reset the disk
+    m_reset_disk
+    
+    ; Write the loading message for the second stage
+    m_write_line loading_msg
+    
+    ; Save the the boot parameters so the kernel can access them
+    m_save_cursor
+    
+    ; Enable the a20 line
+    m_enable_a20
+    
+    ; Read the size of the memory and store at 'boot_info'
+    m_get_memory_size
+    
+    ; Set up and write into memory the interrupt descriptor table
+    m_setup_idt
+    
+    ; Set up and write into memory the global descriptor table
+    m_setup_gdt
+    
+    ; Load the tables
+    m_load_gdt_and_idt
+    
+    ; Enable protected mode
+    m_enable_protected
+    
+    jmp     0x08:stage_2_bootloader_32  ; Set CS to the code segment of the kernel in the GDT
 
 ; If there is a floppy disk error or a boot error, the call this function
 boot_error:
-	m_write_line disk_error_msg		; Print the disk error message
-	m_reboot						; Reboot
+    m_reboot_with_msg disk_error_msg    ; Print the disk error message
+                                        ; Reboot
 
 %include 'functions.asm'
 
 idt_descriptor:
-	dw 0x0000						; 256 entries of 8 bytes for the interrupt table
-	dd 0x0000						; The location of the table, at 0x0000:0x0000
-	
+    dw 0x0000                           ; 256 entries of 8 bytes for the interrupt table
+    dd 0x0000                           ; The location of the table, at 0x0000:0x0000
+    
 gdt_descriptor:
-	dw 0x0017						; 3 tables of 8 bytes total (minus 1)
-	dd 0x0800						; The location of the 3 tables, at 0x0000:0x0800, just bellow the IDT
+    dw 0x0017                           ; 3 tables of 8 bytes total (minus 1)
+    dd 0x0800                           ; The location of the 3 tables, at 0x0000:0x0800, just bellow the IDT
 
-kernel_filename		db "KERNEL  BIN", 0
-disk_error_msg		db "Disk error", 0
-loading_msg			db "Loading: 2nd stage bootloader", 0
-reboot_msg			db "Press any key to reboot", 0
-a20_error			db "a20 line not initialised", 0
+kernel_filename     db "KERNEL  BIN", 0
+disk_error_msg      db "Disk error", 0
+loading_msg         db "Loading: 2nd stage bootloader", 0
+reboot_msg          db "Press any key to reboot", 0
+a20_error           db "a20 line not initialised", 0
 
-loading_kernel		db "Loading: Kernel", 0x0A, 0
+loading_kernel      db "Loading: Kernel", 0x0A, 0
 
-memory_map_error	db "Error getting memory map from BIOS INT 0x15 0xE820", 0
+memory_map_error    db "Error getting memory map from BIOS INT 0x15 0xE820", 0
 
 ; Data storage
-root_sectors		db 0,0
-root_start			db 0,0
-file_start			db 0,0
+root_sectors        db 0,0
+root_start          db 0,0
+file_start          db 0,0
 
-kernel_size			db 0,0
+kernel_size         db 0,0
 
 ; Now in 32bit mode
-	[bits	32]
+    [bits    32]
 
 stage_2_bootloader_32:
-	m_set_up_segments
-	
-	lea		esi, [loading_kernel]
-	call	print_string_32
-	
-	; Move kernel to target location
-	mov		esi, kernel_load_location
-	mov		edi, kernel_target_location
-	xor		ecx, ecx					; Zero out ECX for the kernel size
-	mov		cx, word [kernel_size]
-	shr		cx, 2						; Divide by 4 as now copying 4 bytes at a time
-	cld
-	rep		movsd
-	
-	jmp		kernel_target_location		; Jump to the kernel, shouldn't return
+    m_set_up_segments
+    
+    lea     esi, [loading_kernel]
+    call    print_string_32
+    
+    ; Move kernel to target location
+    mov     esi, kernel_load_location
+    mov     edi, kernel_target_location
+    xor     ecx, ecx                    ; Zero out ECX for the kernel size
+    mov     cx, word [kernel_size]
+    shr     cx, 2                       ; Divide by 4 as now copying 4 bytes at a time
+    cld
+    rep     movsd
+    
+    jmp     kernel_target_location      ; Jump to the kernel, shouldn't return
 
 %include '32bit_functions.asm'
 
 times (3 * 512) - ($ - $$) db 0
 
-	[absolute	boot_params_location]
+    [absolute   boot_params_location]
 
 boot_parameters:
-	.signature				resw 1
-	.cursor_pos_x			resb 1
-	.cursor_pos_y			resb 1
-	.memory_lower			resw 1
-	.memory_upper			resw 1
-	.memory_map_address		resd 1
-	.memory_map_length		resw 1
-	.kernel_len				resd 1
-	
+    .signature          resw 1
+    .cursor_pos_x       resb 1
+    .cursor_pos_y       resb 1
+    .memory_lower       resw 1
+    .memory_upper       resw 1
+    .memory_map_address resd 1
+    .memory_map_length  resw 1
+    .kernel_len         resd 1
--- src/bootloader/32bit_functions.asm
+++ src/bootloader/32bit_functions.asm
@@ -1,102 +1,102 @@
-	[bits	32]
-	
-%define		VIDMEM		0xB8000		; Video memory mapped
-%define		COLUMNS		80			; Width and height of screen (80x25)
-%define		ROWS		25
-%define		CHAR_ATTRIB	0x02		; Character attribute
-	
+    [bits    32]
+    
+%define     VIDMEM          0xB8000 ; Video memory mapped
+%define     COLUMNS         80      ; Height of screen
+%define     ROWS            25      ; Width of screen
+%define     CHAR_ATTRIB     0x02    ; Character attribute
+    
 ; Prints a character
 ; Input:
-;	BL - The character to print
+;   BL - The character to print
 print_char_32:
-	pusha
-	mov		edi, VIDMEM				; Let EDI point to the start of video memory
-	xor		eax, eax				; Clear EAX
-	
-	mov		ecx, COLUMNS
-	mov		al, byte [boot_parameters.cursor_pos_y]	; Get the y position
-	mul		ecx						; Multiply by the number of columns (EAX = y * COLUMNS)
-	
-	xor		ecx, ecx
-	mov		cl, byte [boot_parameters.cursor_pos_x]	; Get the x position
-	add		eax, ecx				; Add the x position to (y * COLUMS)
-	shl		eax, 1					; Multiply by 2 as 2 bytes per character
-	
-	add		edi, eax				; Add the offset to the video memory address
-	
-	cmp		bl, 0x0A				; Is it a new line
-	je		short .new_line
-	
-	mov		dl, bl					; Get the character to print
-	mov		dh, CHAR_ATTRIB			; Add the character attribute
-	mov		word [edi], dx			; Write to the video memory
-	
-	inc		byte [boot_parameters.cursor_pos_x]		; Increment the x position
-	jmp		short .print_done
-	
+    pusha
+    mov     edi, VIDMEM                             ; Let EDI point to the start of video memory
+    xor     eax, eax                                ; Clear EAX
+    
+    mov     ecx, COLUMNS
+    mov     al, byte [boot_parameters.cursor_pos_y] ; Get the y position
+    mul     ecx                                     ; Multiply by the number of columns (EAX = y * COLUMNS)
+    
+    xor     ecx, ecx
+    mov     cl, byte [boot_parameters.cursor_pos_x] ; Get the x position
+    add     eax, ecx                                ; Add the x position to (y * COLUMS)
+    shl     eax, 1                                  ; Multiply by 2 as 2 bytes per character
+    
+    add     edi, eax                                ; Add the offset to the video memory address
+    
+    cmp     bl, 0x0A                                ; Is it a new line
+    je      short .new_line
+    
+    mov     dl, bl                                  ; Get the character to print
+    mov     dh, CHAR_ATTRIB                         ; Add the character attribute
+    mov     word [edi], dx                          ; Write to the video memory
+    
+    inc     byte [boot_parameters.cursor_pos_x]     ; Increment the x position
+    jmp     short .print_done
+    
 .new_line:
-	mov		byte [boot_parameters.cursor_pos_x], 0	; Set cursor to beginning of line
-	inc		byte [boot_parameters.cursor_pos_y]		; Increment new line
-	
+    mov     byte [boot_parameters.cursor_pos_x], 0  ; Set cursor to beginning of line
+    inc     byte [boot_parameters.cursor_pos_y]     ; Increment new line
+    
 .print_done:
-	popa
-	ret
+    popa
+    ret
 
 ; Print a null terminated string to the screen
 ; Input:
-;	ESI - Pointer to the string to print
+;   ESI - Pointer to the string to print
 print_string_32:
-	pusha
+    pusha
 
 .print_loop:
-	mov		bl, byte [esi]		; Get the character to print
-	cmp		bl, 0				; Is it null
-	je		short .print_end			; Then finish
-	
-	call	print_char_32		; Print the character
-	
-	inc		esi					; Increment to next character
-	jmp		short .print_loop
-	
+    mov     bl, byte [esi]                          ; Get the character to print
+    cmp     bl, 0                                   ; Is it null
+    je      short .print_end                        ; Then finish
+    
+    call    print_char_32                           ; Print the character
+    
+    inc     esi                                     ; Increment to next character
+    jmp     short .print_loop
+    
 .print_end:
-	mov		bh, byte [boot_parameters.cursor_pos_y]
-	mov		bl, byte [boot_parameters.cursor_pos_x]
-	call	update_cursor		; Update the cursor
-	
-	popa
-	ret
+    mov     bh, byte [boot_parameters.cursor_pos_y]
+    mov     bl, byte [boot_parameters.cursor_pos_x]
+    call    update_cursor                           ; Update the cursor
+    
+    popa
+    ret
 
 ; Update the cursors new position
 ; Input:
-;	BH - y position
-;	BL - x position
+;   BH - y position
+;   BL - x position
 update_cursor:
-	pusha
-	
-	xor		eax, eax
-	mov		ecx, COLUMNS
-	mov		al, bh
-	mul		ecx
-	add		al, bl
-	mov		ebx, eax
-	
-	mov		al, 0x0f
-	mov		dx, 0x03D4
-	out		dx, al
+    pusha
+    
+    xor     eax, eax
+    mov     ecx, COLUMNS
+    mov     al, bh
+    mul     ecx
+    add     al, bl
+    mov     ebx, eax
+    
+    mov     al, 0x0f
+    mov     dx, 0x03D4
+    out     dx, al
 
-	mov		al, bl
-	mov		dx, 0x03D5
-	out		dx, al
-	
-	xor		eax, eax
+    mov     al, bl
+    mov     dx, 0x03D5
+    out     dx, al
+    
+    xor     eax, eax
 
-	mov		al, 0x0e
-	mov		dx, 0x03D4
-	out		dx, al
+    mov     al, 0x0e
+    mov     dx, 0x03D4
+    out     dx, al
 
-	mov		al, bh
-	mov		dx, 0x03D5
-	out		dx, al
+    mov     al, bh
+    mov     dx, 0x03D5
+    out     dx, al
 
-	popa
-	ret
+    popa
+    ret
