4d9b9633108aa89b2d3daaeb7c64a435ab4f79c7
ED <ed.dean515@gmail.com>
2019-09-17 18:24:27 +0100

    Added IDT unit and run-time tests

--- test/mock/kernel/mocking.zig
+++
@@ -1,6 +0,0 @@
-pub const arch = @import("arch_mock.zig");
-pub const gdt = @import("gdt_mock.zig");
-pub const log = @import("log_mock.zig");
-pub const mem = @import("mem_mock.zig");
-pub const panic = @import("panic_mock.zig");
-pub const vga = @import("vga_mock.zig");
