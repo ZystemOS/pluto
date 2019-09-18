83d7e5380e6aee7c015f27638c8928c201c0cd0d
SamTebbs33 <samuel.tebbs@gmail.com>
2019-09-18 18:42:08 +0100

    feedback

--- src/kernel/arch/x86/paging.zig
+++ src/kernel/arch/x86/paging.zig
@@ -1,83 +1,124 @@
 const std = @import("std");
+const expectEqual = std.testing.expectEqual;
+const expect = std.testing.expect;
 const builtin = @import("builtin");
-const panic = @import("../../kmain.zig").panic;
-const arch = @import("../../arch.zig").internals;
+const panic = @import("../../panic.zig");
+const arch = @import("arch.zig");
 const isr = @import("isr.zig");
-const assert = std.debug.assert;
 const MemProfile = @import("../../mem.zig").MemProfile;
 const tty = @import("../../tty.zig");
-const testing = @import("std").testing;
-const expectEqual = testing.expectEqual;
-const expect = testing.expect;
 
+/// An array of directory entries and page tables. Forms the first level of paging and covers the entire 4GB memory space.
 const Directory = packed struct {
+    /// The directory entries.
     entries: [ENTRIES_PER_DIRECTORY]DirectoryEntry,
+
+    /// The tables allocated for the directory. This is ignored by the CPU.
     tables: [ENTRIES_PER_DIRECTORY]?*Table,
 };
 
+/// An array of table entries. Forms the second level of paging and covers a 4MB memory space.
 const Table = packed struct {
+    /// The table entries.
     entries: [ENTRIES_PER_TABLE]TableEntry,
 };
 
+/// All errors that can be thrown by paging functions.
 const PagingError = error{
+    /// Physical addresses are invalid (definition is up to the function).
     InvalidPhysAddresses,
+
+    /// Virtual addresses are invalid (definition is up to the function).
     InvalidVirtAddresses,
+
+    /// Physical and virtual addresses don't cover spaces of the same size.
     PhysicalVirtualMismatch,
+
+    /// Physical addressses aren't aligned by page size.
     UnalignedPhysAddresses,
+
+    /// Virtual addressses aren't aligned by page size.
     UnalignedVirtAddresses,
 };
 
-const PhysAddr = u10;
+/// An entry within a directory. References a single page table.
+/// Bit 0: Present. Set if present in physical memory.
+///        When not set, all remaining 31 bits are ignored and available for use.
+/// Bit 1: Writable. Set if writable.
+/// Bit 2: User. Set if accessible by user mode.
+/// Bit 3: Write through. Set if write-through caching is enabled.
+/// Bit 4: Cache disabled. Set if caching is disabled for this table.
+/// Bit 5: Accessed. Set by the CPU when the table is accessed. Not cleared by CPU.
+/// Bit 6: Zero.
+/// Bit 7: Page size. Set if this entry covers a single 4MB page rather than 1024 4KB pages.
+/// Bit 8: Ignored.
+/// Bits 9-11: Ignored and available for use by kernel.
+/// Bits 12-31: The 4KB aligned physical address of the corresponding page table.
+///             Must be 4MB aligned if the page size bit is set.
 const DirectoryEntry = u32;
+
+/// An entry within a page table. References a single page.
+/// Bit 0: Present. Set if present in physical memory.
+///        When not set, all remaining 31 bits are ignored and available for use.
+/// Bit 1: Writable. Set if writable.
+/// Bit 2: User. Set if accessible by user mode.
+/// Bit 3: Write through. Set if write-through caching is enabled.
+/// Bit 4: Cache disabled. Set if caching is disabled for this page.
+/// Bit 5: Accessed. Set by the CPU when the page is accessed. Not cleared by CPU.
+/// Bit 6: Dirty. Set by the CPU when the page has been written to. Not cleared by the CPU.
+/// Bit 7: Zero.
+/// Bit 8: Global. Set if the cached address for this page shouldn't be updated when cr3 is changed.
+/// Bits 9-11: Ignored and available for use by the kernel.
+/// Bits 12-31: The 4KB aligned physical address mapped to this page.
 const TableEntry = u32;
 
+const ENTRIES_PER_DIRECTORY: u32 = 1024;
+const ENTRIES_PER_TABLE: u32 = 1024;
+
+const PAGE_SIZE_4MB: u32 = 0x400000;
+const PAGE_SIZE_4KB: u29 = PAGE_SIZE_4MB / 1024;
+const PAGE_SIZE: u32 = PAGE_SIZE_4KB;
+const PAGES_PER_DIR_ENTRY: u32 = PAGE_SIZE_4MB / PAGE_SIZE;
+const PAGES_PER_DIR: u32 = ENTRIES_PER_DIRECTORY * PAGES_PER_DIR_ENTRY;
+const PHYS_ADDR_OFFSET: u32 = 22;
+
+const DENTRY_PRESENT: u32 = 0x1;
+const DENTRY_WRITABLE: u32 = 0x2;
+const DENTRY_USER: u32 = 0x4;
+const DENTRY_WRITE_THROUGH: u32 = 0x8;
+const DENTRY_CACHE_DISABLED: u32 = 0x10;
+const DENTRY_ACCESSED: u32 = 0x20;
+const DENTRY_ZERO: u32 = 0x40;
+const DENTRY_4MB_PAGES: u32 = 0x80;
+const DENTRY_IGNORED: u32 = 0x100;
+const DENTRY_AVAILABLE: u32 = 0xE00;
+const DENTRY_PAGE_ADDR: u32 = 0xFFFFF000;
+
+const TENTRY_PRESENT: u32 = 0x1;
+const TENTRY_WRITABLE: u32 = 0x2;
+const TENTRY_USER: u32 = 0x4;
+const TENTRY_WRITE_THROUGH: u32 = 0x8;
+const TENTRY_CACHE_DISABLED: u32 = 0x10;
+const TENTRY_ACCESSED: u32 = 0x20;
+const TENTRY_DIRTY: u32 = 0x40;
+const TENTRY_ZERO: u32 = 0x80;
+const TENTRY_GLOBAL: u32 = 0x100;
+const TENTRY_AVAILABLE: u32 = 0xE00;
+const TENTRY_PAGE_ADDR: u32 = 0xFFFFF000;
+
 extern var KERNEL_ADDR_OFFSET: *u32;
-
-const ENTRIES_PER_DIRECTORY = 1024;
-const ENTRIES_PER_TABLE = 1024;
-
-const PAGE_SIZE_4MB = 0x400000;
-const PAGE_SIZE_4KB = PAGE_SIZE_4MB / 1024;
-const PAGE_SIZE = PAGE_SIZE_4KB;
-const PAGES_PER_DIR_ENTRY = PAGE_SIZE_4MB / PAGE_SIZE;
-const PAGES_PER_DIR = ENTRIES_PER_DIRECTORY * PAGES_PER_DIR_ENTRY;
-const PHYS_ADDR_OFFSET = 22;
-
-const DENTRY_PRESENT = 0x1;
-const DENTRY_WRITABLE = 0x2;
-const DENTRY_USER = 0x4;
-const DENTRY_WRITE_THROUGH = 0x8;
-const DENTRY_CACHE_DISABLED = 0x10;
-const DENTRY_ACCESSED = 0x20;
-const DENTRY_ZERO = 0x40;
-const DENTRY_4MB_PAGES = 0x80;
-const DENTRY_IGNORED = 0x100;
-const DENTRY_AVAILABLE = 0xE00;
-const DENTRY_PAGE_ADDR = 0xFFFFF000;
-
-const TENTRY_PRESENT = 0x1;
-const TENTRY_WRITABLE = 0x2;
-const TENTRY_USER = 0x4;
-const TENTRY_WRITE_THROUGH = 0x8;
-const TENTRY_CACHE_DISABLED = 0x10;
-const TENTRY_ACCESSED = 0x20;
-const TENTRY_DIRTY = 0x40;
-const TENTRY_ZERO = 0x80;
-const TENTRY_GLOBAL = 0x100;
-const TENTRY_AVAILABLE = 0xE00;
-const TENTRY_PAGE_ADDR = 0xFFFFF000;
 
 ///
 /// Convert a virtual address to its physical counterpart by subtracting the kernel virtual offset from the virtual address.
 ///
 /// Arguments:
-///     virt: integer or pointer - The virtual address to convert.
+///     IN virt: var - The virtual address to covert. Either an integer or pointer.
 ///
 /// Return: @typeOf(virt)
 ///     The physical address.
 ///
 inline fn virtToPhys(virt: var) @typeOf(virt) {
-    const offset = if (builtin.is_test) 0xC000000 else @ptrToInt(&KERNEL_ADDR_OFFSET);
+    const offset = @ptrToInt(&KERNEL_ADDR_OFFSET);
     const T = @typeOf(virt);
     return switch (@typeId(T)) {
         .Pointer => @intToPtr(T, @ptrToInt(virt) - offset),
@@ -90,7 +131,7 @@ inline fn virtToPhys(virt: var) @typeOf(virt) {
 /// Convert a virtual address to an index within an array of directory entries.
 ///
 /// Arguments:
-///     virt: usize - The virtual address to convert.
+///     IN virt: usize - The virtual address to convert.
 ///
 /// Return: usize
 ///     The index into an array of directory entries.
@@ -103,7 +144,7 @@ inline fn virtToDirEntryIdx(virt: usize) usize {
 /// Convert a virtual address to an index within an array of table entries.
 ///
 /// Arguments:
-///     virt: usize - The virtual address to convert.
+///     IN virt: usize - The virtual address to convert.
 ///
 /// Return: usize
 ///     The index into an array of table entries.
@@ -126,7 +167,7 @@ inline fn virtToTableEntryIdx(virt: usize) usize {
 ///
 /// Error: PagingError || std.mem.Allocator.Error
 ///     PagingError.InvalidPhysAddresses - The physical start address is greater than the end.
-///     PagingError.InvalidVirtAddresses - The virtual start address is greater than the end.
+///     PagingError.InvalidVirtAddresses - The virtual start address is greater than the end or is larger than 4GB.
 ///     PagingError.PhysicalVirtualMismatch - The differences between the virtual addresses and the physical addresses aren't the same.
 ///     PagingError.UnalignedPhysAddresses - One or both of the physical addresses aren't page size aligned.
 ///     PagingError.UnalignedVirtAddresses - One or both of the virtual addresses aren't page size aligned.
@@ -150,6 +191,8 @@ fn mapDirEntry(dir: *Directory, virt_start: usize, virt_end: usize, phys_start: 
     }
 
     const entry = virt_start / PAGE_SIZE_4MB;
+    if (entry >= ENTRIES_PER_DIRECTORY)
+        return PagingError.InvalidVirtAddresses;
     var dir_entry = &dir.entries[entry];
     dir_entry.* |= DENTRY_PRESENT;
     dir_entry.* |= DENTRY_WRITABLE;
@@ -167,7 +210,7 @@ fn mapDirEntry(dir: *Directory, virt_start: usize, virt_end: usize, phys_start: 
         // Create a table and put the physical address in the dir entry
         table = &(try allocator.alignedAlloc(Table, PAGE_SIZE_4KB, 1))[0];
         @memset(@ptrCast([*]u8, table), 0, @sizeOf(Table));
-        const table_phys_addr: usize = @ptrToInt(virtToPhys(table));
+        const table_phys_addr = @ptrToInt(virtToPhys(table));
         dir_entry.* |= @intCast(u32, DENTRY_PAGE_ADDR & table_phys_addr);
         dir.tables[entry] = table;
     }
@@ -190,8 +233,8 @@ fn mapDirEntry(dir: *Directory, virt_start: usize, virt_end: usize, phys_start: 
 /// Sets the entry to be present, writable, kernel access, write through, cache enabled, non-global and the page address bits.
 ///
 /// Arguments:
-///     entry: *align(1) TableEntry - The entry to map. 1 byte aligned.
-///     phys_addr: usize - The physical address to map the table entry to.
+///     OUT entry: *align(1) TableEntry - The entry to map. 1 byte aligned.
+///     IN phys_addr: usize - The physical address to map the table entry to.
 ///
 /// Error: PagingError
 ///     PagingError.UnalignedPhysAddresses - If the physical address isn't page size aligned.
@@ -237,6 +280,12 @@ fn mapDir(dir: *Directory, phys_start: usize, phys_end: usize, virt_start: usize
     }
 }
 
+///
+/// Called when a page fault occurs.
+///
+/// Arguments:
+///     IN state: *arch.InterruptContext - The CPU's state when the fault occured.
+///
 fn pageFault(state: *arch.InterruptContext) void {
     @panic("Page fault");
 }
@@ -255,18 +304,18 @@ pub fn init(mem_profile: *const MemProfile, allocator: *std.mem.Allocator) void 
     const p_start = std.mem.alignBackward(@ptrToInt(mem_profile.physaddr_start), PAGE_SIZE);
     const p_end = std.mem.alignForward(@ptrToInt(mem_profile.physaddr_end) + mem_profile.fixed_alloc_size, PAGE_SIZE);
 
-    var tmp = allocator.alignedAlloc(Directory, PAGE_SIZE_4KB, 1) catch unreachable;
+    var tmp = allocator.alignedAlloc(Directory, PAGE_SIZE_4KB, 1) catch @panic("Failed to allocate page directory");
     var kernel_directory = @ptrCast(*Directory, tmp.ptr);
     @memset(@ptrCast([*]u8, kernel_directory), 0, @sizeOf(Directory));
 
     // Map in kernel
-    mapDir(kernel_directory, p_start, p_end, v_start, v_end, allocator) catch unreachable;
+    mapDir(kernel_directory, p_start, p_end, v_start, v_end, allocator) catch panic.panicFmt(@errorReturnTrace(), "Failed to map kernel directory");
     const tty_addr = tty.getVideoBufferAddress();
     // If the previous mappping space didn't cover the tty buffer, do so now
     if (v_start > tty_addr or v_end <= tty_addr) {
         const tty_phys = virtToPhys(tty_addr);
         const tty_buff_size = 32 * 1024;
-        mapDir(kernel_directory, tty_phys, tty_phys + tty_buff_size, tty_addr, tty_addr + tty_buff_size, allocator) catch unreachable;
+        mapDir(kernel_directory, tty_phys, tty_phys + tty_buff_size, tty_addr, tty_addr + tty_buff_size, allocator) catch panic.panicFmt(@errorReturnTrace(), "Failed to map vga buffer in kernel directory");
     }
 
     const dir_physaddr = @ptrToInt(virtToPhys(kernel_directory));
@@ -274,7 +323,7 @@ pub fn init(mem_profile: *const MemProfile, allocator: *std.mem.Allocator) void 
         :
         : [addr] "{eax}" (dir_physaddr)
     );
-    isr.registerIsr(14, pageFault) catch unreachable;
+    isr.registerIsr(14, pageFault) catch @panic("Failed to register page fault ISR");
 }
 
 fn checkDirEntry(entry: DirectoryEntry, virt_start: usize, virt_end: usize, phys_start: usize, table: *Table) void {
@@ -310,7 +359,7 @@ fn checkTableEntry(entry: TableEntry, page_phys: usize) void {
 }
 
 test "virtToPhys" {
-    const offset: usize = 0xC000000;
+    const offset: usize = @ptrToInt(&KERNEL_ADDR_OFFSET);
     expectEqual(virtToPhys(offset + 0), 0);
     expectEqual(virtToPhys(offset + 123), 123);
     expectEqual(@ptrToInt(virtToPhys(@intToPtr(*usize, offset + 123))), 123);
@@ -324,7 +373,6 @@ test "virtToDirEntryIdx" {
     expectEqual(virtToDirEntryIdx(PAGE_SIZE_4MB + 1), 1);
     expectEqual(virtToDirEntryIdx(PAGE_SIZE_4MB * 2), 2);
     expectEqual(virtToDirEntryIdx(PAGE_SIZE_4MB * (ENTRIES_PER_DIRECTORY - 1)), ENTRIES_PER_DIRECTORY - 1);
-    expectEqual(virtToDirEntryIdx(PAGE_SIZE_4MB * (ENTRIES_PER_DIRECTORY)), 0);
 }
 
 test "virtToTableEntryIdx" {
