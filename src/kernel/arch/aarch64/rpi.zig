6310e79a60702200fb4281b5815addf80fd7d28e
Sam Tebbs <samuel.tebbs@gmail.com>
2020-06-07 18:30:01 +0100

    Add rpi.zig

---
+++ src/kernel/arch/aarch64/rpi.zig
@@ -0,0 +1,20 @@
+/// The supported aarch64-capable raspberry pi boards
+pub const RaspberryPiBoard = enum {
+    RaspberryPi3,
+    RaspberryPi4,
+
+    pub fn fromPartNumber(part_num: u12) ?RaspberryPiBoard {
+        return switch (part_num) {
+            0xD03 => RaspberryPiBoard.RaspberryPi3,
+            0xD08 => RaspberryPiBoard.RaspberryPi4,
+            else => null,
+        };
+    }
+
+    pub fn mmioAddress(self: @This()) usize {
+        return switch (self) {
+            .RaspberryPi3 => 0x3F000000,
+            .RaspberryPi4 => 0xFE000000,
+        };
+    }
+};
