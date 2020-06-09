64c1413f2d26e99e44ad1e12899399cb100d8f41
Sam Tebbs <samuel.tebbs@gmail.com>
2020-06-09 13:18:23 +0100

    Set board type as payload

--- src/kernel/arch/aarch64/boot.zig
+++ src/kernel/arch/aarch64/boot.zig
@@ -1,6 +1,7 @@
+const rpi = @import("rpi.zig");
 export var kernel_stack: [16 * 1024]u8 align(16) linksection(".bss.stack") = undefined;
 
-extern fn kmain() void;
+extern fn kmain(payload: *const rpi.RaspberryPiBoard) void;
 extern var KERNEL_STACK_END: *usize;
 
 export fn _start() linksection(".text.boot") callconv(.Naked) noreturn {
@@ -10,7 +11,26 @@ export fn _start() linksection(".text.boot") callconv(.Naked) noreturn {
         :
         : [stack_end] "r" (@ptrCast([*]u8, &KERNEL_STACK_END))
     );
+    // Halt all cores other than the primary core, until the kernel has multicore support
+    const core_id = asm ("mrs %[res], mpidr_el1"
+        : [res] "=r" (-> usize)
+    ) & 3;
+    if (core_id != 0) {
+        while (true) {
+            asm volatile ("wfe");
+        }
+    }
 
-    //kmain(0);
+    // The rpi puts the board part number in midr_el1
+    const board = detectBoard();
+
+    kmain(&board);
     while (true) {}
+}
+
+fn detectBoard() rpi.RaspberryPiBoard {
+    const part_number = @truncate(u12, asm ("mrs %[res], midr_el1"
+        : [res] "=r" (-> usize)
+    ) >> 4);
+    return rpi.RaspberryPiBoard.fromPartNumber(part_number) orelse @panic("Unrecognised part number");
 }
