967369fdbf9a81f5f8c6dc78b521d24909bab654
Sam Tebbs <samuel.tebbs@gmail.com>
2020-06-09 13:19:19 +0100

    Add serial

--- src/kernel/arch/aarch64/arch.zig
+++ src/kernel/arch/aarch64/arch.zig
@@ -1,7 +1,9 @@
 const std = @import("std");
 const vmm = @import("../../vmm.zig");
 const mem = @import("../../mem.zig");
+const Serial = @import("../../serial.zig").Serial;
 const rpi = @import("rpi.zig");
+const mmio = @import("mmio.zig");
 /// The type of the payload passed to a virtual memory mapper.
 // TODO: implement
 pub const VmmPayload = usize;
@@ -15,6 +17,58 @@ pub const VMM_MAPPER: vmm.Mapper(VmmPayload) = vmm.Mapper(VmmPayload){ .mapFn = 
 
 // TODO: implement
 pub const KERNEL_VMM_PAYLOAD: VmmPayload = 0;
+
+// The system clock frequency in Hz. This is the default for rpi 1 and 2 so use it for rpi 3 and 4 for consistency
+const SYSTEM_CLOCK: usize = 700000000;
+const UART_BAUD_RATE: usize = 115200;
+
+var mmio_addr: usize = undefined;
+
+pub fn initSerial(board: BootPayload) Serial {
+    mmio_addr = board.mmioAddress();
+    // Disable uart
+    mmio.write(mmio_addr, mmio.Register.UART_CONTROL, 0);
+    // Disable pull up/down for all GPIO pins
+    mmio.write(mmio_addr, mmio.Register.GPIO_PULL, 0);
+    // Disable pull up/down for pins 14 and 15
+    mmio.write(mmio_addr, mmio.Register.GPIO_PULL_CLK, (1 << 14) | (1 << 15));
+    // Apply pull up/down change for pins 14 and 15
+    mmio.write(mmio_addr, mmio.Register.GPIO_PULL_CLK, 0);
+    // Clear pending uart interrupts
+    mmio.write(mmio_addr, mmio.Register.UART_INT_CONTROL, 0x7FF);
+
+    // For models after rpi 2 (those supporting aarch64) the uart clock is dependent on the system clock
+    // Set it to 3Mhz to be consistent with rpi 1 and 2
+    const cmd: []const volatile u32 align(16) = &[_]u32{ 36, 0, 0x38002, 12, 8, 2, SYSTEM_CLOCK, 0, 0 };
+    const cmd_addr = (@intCast(u32, @ptrToInt(&cmd)) & ~@as(u32, 0xF)) | 8;
+    // Wait until the mailbox isn't busy
+    while ((mmio.read(mmio_addr, mmio.Register.MBOX_STATUS) & 0x80000000) != 0) {}
+    mmio.write(mmio_addr, mmio.Register.MBOX_WRITE, cmd_addr);
+    // Wait for the correct response
+    while ((mmio.read(mmio_addr, mmio.Register.MBOX_STATUS) & 0x40000000) != 0 or mmio.read(mmio_addr, mmio.Register.MBOX_READ) != cmd_addr) {}
+
+    // Setup baudrate
+    const divisor: f32 = @intToFloat(f32, SYSTEM_CLOCK) / @intToFloat(f32, 16 * UART_BAUD_RATE);
+    const divisor_int: u32 = @floatToInt(u32, divisor);
+    const divisor_frac: u32 = @floatToInt(u32, (divisor - @intToFloat(f32, divisor_int)) * 64.0 + 0.5);
+    mmio.write(mmio_addr, mmio.Register.UART_BAUD_INT, divisor_int);
+    mmio.write(mmio_addr, mmio.Register.UART_BAUD_FRAC, divisor_frac);
+    // Enable FIFO, 8 bit words, 1 stop bit and no parity bit
+    mmio.write(mmio_addr, mmio.Register.UART_LINE_CONTROL, 1 << 4 | 1 << 5 | 1 << 6);
+    // Mask all interrupts
+    mmio.write(mmio_addr, mmio.Register.UART_INT_MASK, 1 << 1 | 1 << 4 | 1 << 5 | 1 << 6 | 1 << 7 | 1 << 8 | 1 << 9 | 1 << 10);
+    // Enable UART0 receive and transmit
+    mmio.write(mmio_addr, mmio.Register.UART_CONTROL, 1 << 0 | 1 << 8 | 1 << 9);
+    return .{
+        .write = uartWriteByte,
+    };
+}
+
+fn uartWriteByte(byte: u8) void {
+    // Wait until the UART is ready to transmit
+    while ((mmio.read(mmio_addr, mmio.Register.UART_FLAGS) & (1 << 5)) != 0) {}
+    mmio.write(mmio_addr, mmio.Register.UART_DATA, byte);
+}
 
 pub fn initMem(payload: BootPayload) std.mem.Allocator.Error!mem.MemProfile {
     // TODO: implement
--- src/kernel/arch/aarch64/mmio.zig
+++ src/kernel/arch/aarch64/mmio.zig
@@ -1,10 +1,21 @@
 const GPIO_OFFSET: usize = 0x200000;
 const UART_OFFSET: usize = GPIO_OFFSET + 0x1000;
+const MBOX_OFFSET: usize = 0xB880;
 
 pub const Register = enum(usize) {
+    GPIO_PULL = GPIO_OFFSET + 0x94,
+    GPIO_PULL_CLK = GPIO_OFFSET + 0x98,
     UART_DATA = UART_OFFSET + 0x00,
     UART_FLAGS = UART_OFFSET + 0x18,
     UART_CONTROL = UART_OFFSET + 0x30,
+    UART_INT_CONTROL = UART_OFFSET + 0x44,
+    UART_BAUD_INT = UART_OFFSET + 0x24,
+    UART_BAUD_FRAC = UART_OFFSET + 0x28,
+    UART_LINE_CONTROL = UART_OFFSET + 0x2C,
+    UART_INT_MASK = UART_OFFSET + 0x38,
+    MBOX_STATUS = MBOX_OFFSET + 0x18,
+    MBOX_WRITE = MBOX_OFFSET + 0x20,
+    MBOX_READ = MBOX_OFFSET + 0x00,
 };
 
 pub fn write(mmio_base: usize, reg: Register, data: u32) void {
