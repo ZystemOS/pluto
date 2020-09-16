f464b19e247ce30a1cc169f1928087b961e70c0c
markfirmware <markfirmware.users.noreply.github.com>
2020-09-16 16:58:27 -0400

    space and ! are displaying on pi

--- src/kernel/arch/aarch64/tty.zig
+++ src/kernel/arch/aarch64/tty.zig
@@ -24,33 +24,52 @@ const Framebuffer = struct {
 };
 
 const Pixel = packed struct {
-    red: u8,
-    green: u8,
     blue: u8,
+    green: u8,
+    red: u8,
     alpha: u8 = 0,
 };
 
 const font = [_][]const u1{
     &[_]u1{},
+    // space
+    &[_]u1{
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+    },
     // !
     &[_]u1{
-        0, 0, 0, 1, 1, 0, 0, 0,
-        0, 0, 0, 1, 1, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 1, 1, 0, 0, 0,
         0, 0, 0, 1, 1, 0, 0, 0,
         0, 0, 0, 1, 1, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 1, 1, 0, 0, 0,
         0, 0, 0, 1, 1, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
     },
 };
 
 var framebuffer: Framebuffer = undefined;
 
 fn writePixel(x: usize, y: usize, pixel: Pixel) void {
-    //  log.debug("Writing pixel {}\n", .{pixel});
-    //  log.debug("    to ({}, {}) at fb 0x{x}, which is address {x}\n", .{ x, y, @ptrToInt(framebuffer.buffer), @ptrToInt(&framebuffer.buffer[y * framebuffer.bytes_per_row + x]) });
-    framebuffer.buffer[y * framebuffer.bytes_per_row + x] = pixel;
+    framebuffer.buffer[y * framebuffer.bytes_per_row / 4 + x] = pixel;
+}
+
+fn clearScreen() void {
+    var y: usize = 0;
+    while (y < framebuffer.height) : (y += 1) {
+        var x: usize = 0;
+        while (x < framebuffer.width) : (x += 1) {
+            writePixel(x, y, BLACK);
+        }
+    }
 }
 
 fn writeChar(x: usize, y: usize, char: u8) void {
@@ -58,17 +77,23 @@ fn writeChar(x: usize, y: usize, char: u8) void {
     if (char < ' ' or char > '~')
         ch = ' ';
     // const bitmap = font[ch - ' '];
-    const bitmap = font[0];
+    var font_index: usize = undefined;
+    if (ch == ' ') {
+        font_index = 1;
+    } else {
+        font_index = 2;
+    }
+    const bitmap = font[font_index];
     const left = x * CHAR_WIDTH;
     const top = y * CHAR_HEIGHT;
     var pixel_x = left;
     var pixel_y = top;
-    for (bitmap) |bit, i| {
+    for (bitmap) |bit| {
         writePixel(pixel_x, pixel_y, if (bit == 0) BLACK else WHITE);
         pixel_x += 1;
-        if (pixel_x >= left + CHAR_WIDTH) {
+        if (pixel_x == left + CHAR_WIDTH) {
             pixel_x = left;
-            pixel_y += CHAR_HEIGHT;
+            pixel_y += 1;
         }
     }
 }
@@ -131,7 +156,7 @@ pub fn init(allocator2: *std.heap.FixedBufferAllocator, board: arch.BootPayload)
     };
     const mmio_addr = board.mmioAddress();
     var pkg = mailbox.send(mmio_addr, allocate_fb, allocator) catch |e| panic(@errorReturnTrace(), "Failed to configure TTY: {}\n", .{e});
-    log.debug("Data is {}\n", .{pkg.data[0..]});
+    // log.debug("Data is {}\n", .{pkg.data[0..]});
     defer allocator.free(pkg.data);
     var msg = mailbox.read(mmio_addr);
 
@@ -161,9 +186,9 @@ pub fn init(allocator2: *std.heap.FixedBufferAllocator, board: arch.BootPayload)
     if (pkg.data[23] != @enumToInt(mailbox.Code.RESPONSE_SUCCESS) | 4) panic(null, "GET_BYTES_PER_ROW code wasn't as expected in response\n", .{});
 
     pkg.data[5] &= 0x3fffffff;
-    log.debug("FB is at 0x{x} and is of size {}\n", .{ pkg.data[5], pkg.data[6] });
-    log.debug("bytes per row is {}\n", .{pkg.data[24]});
-    log.debug("Data is {}\n", .{pkg.data[0..]});
+    //  log.debug("FB is at 0x{x} and is of size {}\n", .{ pkg.data[5], pkg.data[6] });
+    //  log.debug("bytes per row is {}\n", .{pkg.data[24]});
+    //  log.debug("Data is {}\n", .{pkg.data[0..]});
 
     framebuffer = .{
         .width = 640,
@@ -175,19 +200,12 @@ pub fn init(allocator2: *std.heap.FixedBufferAllocator, board: arch.BootPayload)
         .text_cursor_y = 0,
         .buffer = @intToPtr([*]Pixel, pkg.data[5]),
     };
-    writePixel(0, 0, WHITE);
-    writePixel(0, 1, WHITE);
-    writePixel(1, 0, WHITE);
-    writePixel(1, 1, WHITE);
-    var i: u32 = 0;
-    while (i < 1000) : (i += 1) {
-        writePixel(i % 640, 100 + i / 640, WHITE);
-    }
+
     return .{
         .print = writeString,
         .setCursor = setCursor,
         .cols = framebuffer.text_columns,
         .rows = framebuffer.text_rows,
-        .clear = null,
+        .clear = clearScreen,
     };
 }
