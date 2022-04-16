3101c5a83cab33e7bc81a6e3dd1c33a7d77b4120
Sam Tebbs <samuel.tebbs@gmail.com>
2022-04-16 23:30:17 +0100

    Add -p to mkdir to avoid failing if dir exists

--- fat32_cp.sh
+++ fat32_cp.sh
@@ -2,16 +2,14 @@
 set -ex
 
 IMAGE_PATH_DIR=$1
-mkdir test/fat32/mnt
+mkdir -p test/fat32/mnt
 whoami
 
 if [ "$(whoami)" = "root" ]; then
-	echo "Am root"
 	mount -o utf8=true $IMAGE_PATH_DIR test/fat32/mnt/
 	cp -r test/fat32/test_files/. test/fat32/mnt/
 	umount test/fat32/mnt/
 else
-	echo "Not root"
 	sudo mount -o utf8=true $IMAGE_PATH_DIR test/fat32/mnt/
 	sudo cp -r test/fat32/test_files/. test/fat32/mnt/
 	sudo umount test/fat32/mnt/
